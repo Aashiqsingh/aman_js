2eadea3ad73fdecdca45799e7edfa707bd78bcd4
Aashiqsingh <aashiqsingh56789@gmail.com>
2025-08-04 23:13:55 +0530

    basic

---
+++ basic/basic1.js
@@ -0,0 +1,32 @@
+// console.log("Hello good ning...");
+// console.info("Information function...");
+// console.warn("Warning function...");
+// console.error("Error function...");
+
+// Datatype : number , string , boolean , undefined , object
+
+
+a = 10;
+console.log("value of a = ",a);
+console.log("type of a = ",typeof(a));
+
+
+b = 12.55;
+console.log("value of b = ",b);
+console.log("type of b = ",typeof(b));
+
+name = "aman";
+console.log("value of name = ",name);
+console.log("type of name = ",typeof(name));
+
+isMarried = true;
+console.log("value of isMarried = ",isMarried);
+console.log("type of isMarried = ",typeof(isMarried));
+
+let d;
+console.log("value of d = ",d);
+console.log("type of d = ",typeof(d));
+
+e = null;
+console.log("value of e = ",e);
+console.log("type of e = ",typeof(e));
---
+++ operator/operator.js
@@ -0,0 +1,25 @@
+var a = "20";
+var b = "20";
+
+
+
+// console.log(a == b);
+// console.log(a === b);
+// console.log(a !== b);
+
+
+
+
+
+
+
+
+
+
+// console.log("Addition = ",a + b);
+// console.log("Subtraction = ",a - b);
+// console.log("Multiplication = ",a * b);
+// console.log("Division = ",7/2);
+// console.log("Modulus = ",a % b);
+
+// console.log(3**4);
